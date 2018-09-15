e24f30b7afc261e2036e9d46674c43de37bcb3c2
David Sn <divad.nnamtdeis@gmail.com>
2018-09-16 00:16:12 +0200

    Fix syntax error on older webview versions

--- app/src/main/assets/webview.js
+++ app/src/main/assets/webview.js
@@ -1,8 +1,5 @@
 console.log("init webview injection");
-if (!document.querySelector('meta[name="theme-color"]')) {
-    console.log("page is not a webapp!");
-    return;
+if (document.querySelector('meta[name="theme-color"]')) {
+    var navbar = document.querySelector('div[ng-controller="NavbarCtrl"]');
+    navbar.remove(navbar);
 }
-
-var navbar = document.querySelector('div[ng-controller="NavbarCtrl"]');
-navbar.remove(navbar);
