56a7cf77953a6748310c9175868fcafde3546b10
David Sn <divad.nnamtdeis@gmail.com>
2018-09-22 15:41:40 +0200

    Remove logo and title from registration page

--- app/src/main/assets/webview.js
+++ app/src/main/assets/webview.js
@@ -1,5 +1,20 @@
-if (document.querySelector('meta[name="theme-color"]')) {
+if (document.querySelector('html[ng-app="kkmApp"]')) {
     var navbar = document.querySelector('div[ng-controller="NavbarCtrl"]');
     navbar.remove(navbar);
+
+    // remove logo and title
+    if (document.querySelector('div[layout-fill="layout-fill"]')) {
+        var form = document.querySelector('div[layout-fill="layout-fill"]');
+        var logo = form.querySelector('img[src="img/kkm-logo.png"]');
+        var title = form.querySelector('div[class="md-display-1"]');
+
+        // remove useless breakline tags
+        logo.previousElementSibling.remove(logo.previousElementSibling);
+        title.nextElementSibling.remove(title.nextElementSibling);
+
+        logo.remove(logo);
+        title.remove(title);
+    }
+
     ScriptInjector.callback();
 }
