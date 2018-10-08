2a23f27ed2005b32538d26ad416a845fcc7b7301
David Sn <divad.nnamtdeis@gmail.com>
2018-10-08 16:07:43 +0200

    Add basic splash activity

---
+++ app/src/main/java/de/codebucket/mkkm/activity/SplashActivity.java
@@ -0,0 +1,14 @@
+package de.codebucket.mkkm.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
