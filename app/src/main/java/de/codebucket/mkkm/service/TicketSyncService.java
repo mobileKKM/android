01aeb6d2777718e86b26ee0a80884ecc6ec4aa29
David Sn <divad.nnamtdeis@gmail.com>
2018-10-10 22:51:53 +0200

    Add stub sync adapter

---
+++ app/src/main/java/de/codebucket/mkkm/service/TicketSyncService.java
@@ -0,0 +1,42 @@
+package de.codebucket.mkkm.service;
+
+import android.accounts.Account;
+import android.app.Service;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SyncResult;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class TicketSyncService extends Service {
+
+    private static final Object sSyncAdapterLock = new Object();
+    private static TicketSyncAdapter sSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+        synchronized (sSyncAdapterLock) {
+            if (sSyncAdapter == null)
+                sSyncAdapter = new TicketSyncAdapter(getApplicationContext(), true);
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSyncAdapter.getSyncAdapterBinder();
+    }
+
+    public class TicketSyncAdapter extends AbstractThreadedSyncAdapter {
+
+        public TicketSyncAdapter(Context context, boolean autoInitialize) {
+            super(context, autoInitialize);
+        }
+
+        @Override
+        public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+
+        }
+    }
+}
