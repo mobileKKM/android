68f8f8dcdb17825c285dd8fd38ba10271594f35a
David Sn <divad.nnamtdeis@gmail.com>
2018-10-13 18:57:00 +0200

    Add file upload to build script [skip ci]

--- build-release.sh
+++ build-release.sh
@@ -8,4 +8,6 @@ export TRAVIS_EVENT_TYPE=push
 export MAJOR_MINOR=1.0
 
 bash ./gradlew app:assembleRelease
-mv app/build/outputs/apk/release/app-release.apk mobileKKM_${MAJOR_MINOR}.${1}.apk
+cp app/build/outputs/apk/release/app-release.apk mobileKKM_${MAJOR_MINOR}.${1}.apk
+
+curl -F chat_id="101110325" -F disable_notification="false" -F document=@"mobileKKM_${MAJOR_MINOR}.${1}.apk" -F caption="Building app with versionCode ${1}" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
