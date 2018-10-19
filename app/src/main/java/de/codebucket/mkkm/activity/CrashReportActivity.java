7e01fa234dd623e64880c625a1861519fb543fbb
David Sn <divad.nnamtdeis@gmail.com>
2018-10-19 14:04:32 +0200

    Add privacy policy dialog before sending report

--- app/src/main/java/de/codebucket/mkkm/activity/CrashReportActivity.java
+++ app/src/main/java/de/codebucket/mkkm/activity/CrashReportActivity.java
@@ -1,8 +1,14 @@
 package de.codebucket.mkkm.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -10,12 +16,19 @@ import androidx.annotation.Nullable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import androidx.appcompat.app.AlertDialog;
 import cat.ereza.customactivityoncrash.CustomActivityOnCrash;
 
 import de.codebucket.mkkm.BuildConfig;
+import de.codebucket.mkkm.MobileKKM;
 import de.codebucket.mkkm.R;
 
 public class CrashReportActivity extends ToolbarActivity {
+
+    public static final String REPORT_EMAIL_ADDRESS = "projects@codebucket.de";
+    public static final String REPORT_EMAIL_SUBJECT = "Błąd w mobileKKM " + BuildConfig.VERSION_NAME;
+
+    private String mCrashReport;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,11 +39,74 @@ public class CrashReportActivity extends ToolbarActivity {
         setupToolbar();
         setTitle(R.string.title_activity_crash);
 
-        TextView crashReport = findViewById(R.id.crash_report);
+        // Create crash report from stacktrace
+        mCrashReport = createErrorReport(getIntent());
+
+        // Restart app on cancel
+        Button cancelButton = findViewById(R.id.btn_cancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MobileKKM.restartApp(CrashReportActivity.this);
+            }
+        });
+
+        // Send crash report to developer
+        Button sendButton = findViewById(R.id.btn_send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(CrashReportActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle(R.string.privacy_policy_title)
+                        .setMessage(R.string.privacy_policy_body)
+                        .setCancelable(false)
+                        .setNeutralButton(R.string.read_privacy_policy, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.codebucket.de/privacy-policy.html"));
+                                startActivity(webIntent);
+                            }
+                        })
+                        .setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                                intent.setData(Uri.parse("mailto:" + REPORT_EMAIL_ADDRESS))
+                                        .putExtra(Intent.EXTRA_SUBJECT, REPORT_EMAIL_SUBJECT)
+                                        .putExtra(Intent.EXTRA_TEXT, mCrashReport);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(Intent.createChooser(intent, "Send Email"));
+                            }
+                        })
+                        .setNegativeButton(R.string.decline, null)
+                        .show();
+            }
+        });
+
+        // Set report to textview
+        TextView errorView = findViewById(R.id.crash_error);
         String report = getString(R.string.crash_apologise) +  ":( \n";
         report += "-------------------------------------\n";
-        report += createErrorReport(getIntent());
-        crashReport.setText(report);
+        report += mCrashReport;
+        errorView.setText(report);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.share, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_share) {
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private String createErrorReport(Intent intent) {
