39cda9397221e9431f5ba2a0d405d622bf1b5dd4
David Sn <divad.nnamtdeis@gmail.com>
2018-10-23 09:19:36 +0200

    Use preference screen for backup activity

--- app/src/main/java/de/codebucket/mkkm/activity/BackupActivity.java
+++ app/src/main/java/de/codebucket/mkkm/activity/BackupActivity.java
@@ -1,6 +1,13 @@
 package de.codebucket.mkkm.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.ViewStub;
+
+import androidx.fragment.app.FragmentManager;
+
+import com.takisoft.preferencex.PreferenceCategory;
+import com.takisoft.preferencex.PreferenceFragmentCompat;
 
 import de.codebucket.mkkm.R;
 
@@ -9,13 +16,45 @@ public class BackupActivity extends ToolbarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_backup);
+        setContentView(R.layout.activity_settings);
 
         // Set up action bar
         setupToolbar();
         setTitle(R.string.title_activity_backup);
 
+        ViewStub stub = findViewById(R.id.container_stub);
+        stub.inflate();
+
+        if (savedInstanceState == null) {
+            // Display the fragment as the main content.
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container_content, new BackupFragment())
+                    .commit();
+        }
+
         // Enable back arrow button to return
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+
+        return true;
+    }
+
+    public static class BackupFragment extends PreferenceFragmentCompat {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public void onCreatePreferencesFix(Bundle savedInstanceState, String rootKey) {
+            addPreferencesFromResource(R.xml.preferences_backup);
+        }
     }
 }
