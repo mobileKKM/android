1547fdb7a855644af7893e6b1458d4d68e64a195
David Sn <divad.nnamtdeis@gmail.com>
2018-10-24 12:51:38 +0200

    Open file chooser on backup options selection

--- app/src/main/java/de/codebucket/mkkm/activity/BackupActivity.java
+++ app/src/main/java/de/codebucket/mkkm/activity/BackupActivity.java
@@ -1,14 +1,28 @@
 package de.codebucket.mkkm.activity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.ViewStub;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.preference.Preference;
 
 import com.takisoft.preferencex.PreferenceFragmentCompat;
 
 import de.codebucket.mkkm.R;
 
 public class BackupActivity extends ToolbarActivity {
+
+    private static final int OPEN_FILE_RESULT_CODE = 98;
+    private static final int SAVE_FILE_RESULT_CODE = 99;
+
+    private static final int REQUEST_READ_PERMISSION_CODE = 198;
+    private static final int REQUEST_WRITE_PERMISSION_CODE = 199;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +56,88 @@ public class BackupActivity extends ToolbarActivity {
         return true;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_READ_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showOpenFileSelector();
+            } else {
+                Toast.makeText(this, "no permission", Toast.LENGTH_LONG).show();
+            }
+        } else if (requestCode == REQUEST_WRITE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showSaveFileSelector();
+            } else {
+                Toast.makeText(this, "no permission", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void openFileWithPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            showOpenFileSelector();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE }, REQUEST_READ_PERMISSION_CODE);
+        }
+    }
+
+    private void saveFileWithPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            showSaveFileSelector();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE }, REQUEST_WRITE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void showOpenFileSelector() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/json");
+        startActivityForResult(intent, OPEN_FILE_RESULT_CODE);
+    }
+
+    private void showSaveFileSelector() {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/json");
+        startActivityForResult(intent, SAVE_FILE_RESULT_CODE);
+    }
+
     public static class BackupFragment extends PreferenceFragmentCompat {
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+
+            // Import options
+            Preference backupImport = findPreference("backup_import");
+            Preference backupRestore = findPreference("backup_restore");
+
+            backupImport.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    ((BackupActivity) getActivity()).openFileWithPermissions();
+                    return true;
+                }
+            });
+
+            // Export options
+            Preference backupExport = findPreference("backup_export");
+
+            backupExport.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    ((BackupActivity) getActivity()).saveFileWithPermissions();
+                    return true;
+                }
+            });
         }
 
         @Override
