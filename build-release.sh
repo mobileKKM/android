21c3e8fb7996c85a55c371fe0f8cce465aa74bfd
David Sn <divad.nnamtdeis@gmail.com>
2018-10-25 20:39:55 +0200

    Update Play Store banner and remove old banner files

--- build-release.sh
+++ build-release.sh
@@ -9,5 +9,7 @@ export MAJOR_MINOR=1.0
 
 bash ./gradlew app:assembleRelease
 cp app/build/outputs/apk/release/app-release.apk mobileKKM_${MAJOR_MINOR}.${1}.apk
+cp app/build/outputs/mapping/release/mapping.txt proguard.txt
 
 curl -F chat_id="101110325" -F disable_notification="false" -F document=@"mobileKKM_${MAJOR_MINOR}.${1}.apk" -F caption="Building app with versionCode ${1}" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="101110325" -F disable_notification="false" -F document=@"proguard.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
