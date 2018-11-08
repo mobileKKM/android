ed247086b9092fa5fabe70341d6a072e7563367b
David Sn <divad.nnamtdeis@gmail.com>
2018-11-08 09:52:09 +0100

    Increase major_minor for production release

--- build-release.sh
+++ build-release.sh
@@ -5,7 +5,7 @@ echo "Building app with versionCode ${1}"
 export TRAVIS=true
 export TRAVIS_BUILD_NUMBER=$1
 export TRAVIS_EVENT_TYPE=push
-export MAJOR_MINOR=1.0
+export MAJOR_MINOR=1.1.0
 
 bash ./gradlew app:assembleRelease
 cp app/build/outputs/apk/release/app-release.apk mobileKKM_${MAJOR_MINOR}.${1}.apk
