6dd291123fcf34c6c61dc5746f9eb1d18734fed8
David Sn <divad.nnamtdeis@gmail.com>
2019-04-06 14:51:15 +0200

    Remove job for service and notification channels
    TODO: Re-add notification channels

--- app/src/main/java/de/codebucket/mkkm/MobileKKM.java
+++ app/src/main/java/de/codebucket/mkkm/MobileKKM.java
@@ -5,9 +5,6 @@ import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.ProgressDialog;
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
@@ -25,10 +22,8 @@ import java.util.UUID;
 import cat.ereza.customactivityoncrash.config.CaocConfig;
 
 import de.codebucket.mkkm.activity.CrashReportActivity;
+import de.codebucket.mkkm.api.SessionHandler;
 import de.codebucket.mkkm.database.AppDatabase;
-import de.codebucket.mkkm.login.LoginHelper;
-import de.codebucket.mkkm.service.TicketExpiryCheckService;
-import de.codebucket.mkkm.util.Const;
 import de.codebucket.mkkm.util.RuntimeHelper;
 
 public class MobileKKM extends Application {
@@ -39,7 +34,7 @@ public class MobileKKM extends Application {
     private static MobileKKM instance;
     private static SharedPreferences preferences;
     private static AppDatabase database;
-    private static LoginHelper loginHelper;
+    private static SessionHandler sessionHandler;
 
     private static final long WAIT_BEFORE_RESTART = 1000;
 
@@ -52,7 +47,7 @@ public class MobileKKM extends Application {
         // mKKM webapp uses fingerprint2.js to generate a fingerprint based on user-agent
         preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         if (preferences.getString("fingerprint", null) == null) {
-            preferences.edit().putString("fingerprint", getFingerprint()).apply();
+            preferences.edit().putString("fingerprint", generateFingerprint()).apply();
         }
 
         // Init offline database (first step to native migration)
@@ -60,13 +55,10 @@ public class MobileKKM extends Application {
                 .fallbackToDestructiveMigration()
                 .build();
 
-        // Login handler
-        loginHelper = new LoginHelper(preferences.getString("fingerprint", null));
+        // Session handler
+        sessionHandler = new SessionHandler(getApplicationContext());
 
-        // Create notification channel on Android O
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            createNotificationChannel();
-        }
+        // TODO: Notification channel(s)
 
         // Custom Activity on Crash initialization
         CaocConfig.Builder.create()
@@ -80,23 +72,9 @@ public class MobileKKM extends Application {
                 .apply();
     }
 
-    @TargetApi(Build.VERSION_CODES.O)
-    private void createNotificationChannel() {
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NotificationManager.class);
-        NotificationChannel expiryChannel = new NotificationChannel(Const.ID.EXPIRY_NOTIFICATION_CHANNEL, getString(R.string.notification_channel_expiry), NotificationManager.IMPORTANCE_HIGH);
-        expiryChannel.setDescription(getString(R.string.notification_channel_expiry_desc));
-        expiryChannel.setVibrationPattern(new long[]{0, 100, 100, 100});
-        notificationManager.createNotificationChannel(expiryChannel);
-    }
-
-    public String getFingerprint() {
+    public String generateFingerprint() {
         String deviceId = Secure.getString(getApplicationContext().getContentResolver(), Secure.ANDROID_ID) + SALT;
         return UUID.nameUUIDFromBytes(deviceId.getBytes()).toString().replaceAll("-", "");
-    }
-
-    public void updateFingerprint(String fingerprint) {
-        preferences.edit().putString("fingerprint", fingerprint).apply();
-        loginHelper.updateFingerprint(fingerprint);
     }
 
     public boolean isNetworkConnectivity() {
@@ -111,22 +89,6 @@ public class MobileKKM extends Application {
         return false;
     }
 
-    public void setupTicketService() {
-        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
-        if (preferences.getBoolean("enable_notifications", false)) {
-            if (scheduler.getAllPendingJobs().isEmpty()) {
-                ComponentName service = new ComponentName(this, TicketExpiryCheckService.class);
-                JobInfo info = new JobInfo.Builder(Const.ID.EXPIRY_CHECK_SERVICE_ID, service)
-                        .setPeriodic(6 * 60 * 60 * 1000) // every 6 hours, 4 times a day
-                        .setPersisted(true)
-                        .build();
-                scheduler.schedule(info);
-            }
-        } else {
-            scheduler.cancel(Const.ID.EXPIRY_CHECK_SERVICE_ID);
-        }
-    }
-
     public static MobileKKM getInstance() {
         return instance;
     }
@@ -139,8 +101,8 @@ public class MobileKKM extends Application {
         return database;
     }
 
-    public static LoginHelper getLoginHelper() {
-        return loginHelper;
+    public static SessionHandler getSessionHandler() {
+        return sessionHandler;
     }
 
     public static void restartApp(final Context context) {
