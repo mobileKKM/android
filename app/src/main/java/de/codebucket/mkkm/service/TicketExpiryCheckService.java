47996f366812ca2b9f144ff0e166754e8c4c338c
David Sn <divad.nnamtdeis@gmail.com>
2019-10-08 14:18:21 +0200

    Replace ic_notification_alert with ic_notification_kkm
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- app/src/main/java/de/codebucket/mkkm/service/TicketExpiryCheckService.java
+++ app/src/main/java/de/codebucket/mkkm/service/TicketExpiryCheckService.java
@@ -56,7 +56,7 @@ public class TicketExpiryCheckService extends JobService {
                     String dateTo = DATE_FORMAT.format(ticket.getExpireDate());
 
                     NotificationCompat.Builder builder = new NotificationCompat.Builder(MobileKKM.getInstance(), Const.ID.EXPIRY_NOTIFICATION_CHANNEL);
-                    builder.setSmallIcon(R.drawable.ic_notification_alert)
+                    builder.setSmallIcon(R.drawable.ic_notification_kkm)
                             .setContentTitle(getString(R.string.expiration_notification_title))
                             .setContentText(getString(R.string.expiration_notification_msg, dateFrom, dateTo))
                             .setSound(Uri.parse(prefs.getString("notification_ringtone", null)))
