13fdac4a7919554af0516fa77bd59b94a61fc8bd
David Sn <divad.nnamtdeis@gmail.com>
2019-10-09 22:49:14 +0200

    Attempt to fix navbar not being removed in webview
    Since an update of Chrome, the navbar is not being removed properly.
    This commit is an attempt to fix that by using an interval.
    
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- app/src/main/assets/webview.js
+++ app/src/main/assets/webview.js
@@ -1,20 +1,25 @@
-if (document.querySelector('html[ng-app="kkmApp"]')) {
-    var navbar = document.querySelector('div[ng-controller="NavbarCtrl"]');
-    navbar.remove(navbar);
+var inject = setInterval(removeNavbar, 100);
 
-    // remove logo and title
-    if (document.querySelector('div[layout-fill="layout-fill"]')) {
-        var form = document.querySelector('div[layout-fill="layout-fill"]');
-        var logo = form.querySelector('img[src="img/kkm-logo.png"]');
-        var title = form.querySelector('div[class="md-display-1"]');
+function removeNavbar() {
+    if (document.querySelector('div[ng-controller="NavbarCtrl"]')) {
+        var navbar = document.querySelector('div[ng-controller="NavbarCtrl"]');
+        navbar.remove(navbar);
 
-        // remove useless breakline tags
-        logo.previousElementSibling.remove(logo.previousElementSibling);
-        title.nextElementSibling.remove(title.nextElementSibling);
+        // remove logo and title
+        if (document.querySelector('div[layout-fill="layout-fill"]')) {
+            var form = document.querySelector('div[layout-fill="layout-fill"]');
+            var logo = form.querySelector('img[src="img/kkm-logo.png"]');
+            var title = form.querySelector('div[class="md-display-1"]');
 
-        logo.remove(logo);
-        title.remove(title);
+            // remove useless breakline tags
+            logo.previousElementSibling.remove(logo.previousElementSibling);
+            title.nextElementSibling.remove(title.nextElementSibling);
+
+            logo.remove(logo);
+            title.remove(title);
+        }
+
+        clearInterval(inject);
+        ScriptInjector.callback();
     }
-
-    ScriptInjector.callback();
 }
