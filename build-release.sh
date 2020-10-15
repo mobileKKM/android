effcf5b2eb37939079c1eb9dfe67cae87eb69867
David Sn <divad.nnamtdeis@gmail.com>
2020-10-16 01:55:59 +0200

    Fix semver inside release apk file name
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- build-release.sh
+++ build-release.sh
@@ -22,10 +22,10 @@ fi
 
 echo "Uploading build..."
 
-cp app/build/outputs/apk/release/app-release.apk mobileKKM_${MAJOR_MINOR}.${1}.apk
+cp app/build/outputs/apk/release/app-release.apk mobileKKM_${MAJOR_MINOR}+${1}.apk
 cp app/build/outputs/mapping/release/mapping.txt proguard.txt
 
-curl -F chat_id="101110325" -F disable_notification="false" -F document=@"mobileKKM_${MAJOR_MINOR}.${1}.apk" -F caption="Building app with versionCode ${1}" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="101110325" -F disable_notification="false" -F document=@"mobileKKM_${MAJOR_MINOR}+${1}.apk" -F caption="Building app with versionCode ${1}" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 curl -F chat_id="101110325" -F disable_notification="false" -F document=@"proguard.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 
 echo "Done! Build successfully uploaded via Telegram."
